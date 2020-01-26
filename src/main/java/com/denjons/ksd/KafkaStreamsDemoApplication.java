5ffcd81ca415f9fd2e1d7866e3620d613ac50620
dennis jönsson <dennisjonsson@denniss-MBP.localdomain>
2020-01-26 18:44:42 +0100

    intial commit

---
+++ src/main/java/com/denjons/ksd/KafkaStreamsDemoApplication.java
@@ -0,0 +1,24 @@
+package com.denjons.ksd;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class KafkaStreamsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStreamsDemoApplication.class, args);
+	}
+
+	@Bean
+	public java.util.function.Consumer<KStream<String, String>> process() {
+
+		return input ->
+				input.foreach((key, value) -> {
+					System.out.println("Key: " + key + " Value: " + value);
+				});
+	}
+
+}
